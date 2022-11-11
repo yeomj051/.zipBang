6429e669e4ed659e56807a2b289c2eac0dca2a33
leepungwon <vnddnjs00@naver.com>
2022-11-11 16:05:44 +0900

    backend init

---
+++ BackEnd/DotZipBang/src/main/java/com/zipbang/dotzipbang/DotZipBangApplication.java
@@ -0,0 +1,13 @@
+package com.zipbang.dotzipbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotZipBangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DotZipBangApplication.class, args);
+    }
+
+}
