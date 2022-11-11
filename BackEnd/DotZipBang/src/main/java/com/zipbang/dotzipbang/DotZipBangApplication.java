5fe389050840fce5cd9887cd35d0c5fdf017675a
yeomj051 <yeomj051@naver.com>
2022-11-11 16:17:41 +0900

    민지 임시 수정

--- BackEnd/DotZipBang/src/main/java/com/zipbang/dotzipbang/DotZipBangApplication.java
+++ BackEnd/DotZipBang/src/main/java/com/zipbang/dotzipbang/DotZipBangApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DotZipBangApplication {
-
+    //alswl tnwjd
     public static void main(String[] args) {
         SpringApplication.run(DotZipBangApplication.class, args);
     }
