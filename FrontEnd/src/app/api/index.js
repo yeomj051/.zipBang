3402bbe1da7984ffc36a02dd0dbc84e8d370f0fd
vnddn <vnddnjs00@naver.com>
2022-11-21 14:02:04 +0900

    base url frontend

--- FrontEnd/src/app/api/index.js
+++ FrontEnd/src/app/api/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // local vue api axios instance
 function apiInstance() {
   const instance = axios.create({
-    baseURL: process.env.VUE_APP_API_BASE_URL,
+    baseURL: `http://localhost:9999`,
     headers: {
       "Content-Type": "application/json;charset=utf-8",
     },
